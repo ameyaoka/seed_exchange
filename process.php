7d8f1fd2a54fe97e84625f8b9a3d52d998b1ef6d
ameyaoka <ameyaoka444@gmail.com>
2021-04-19 00:10:02 +0530

    data from user-profile is added to database using php

--- process.php
+++ process.php
@@ -1,11 +1,11 @@
-?php
+<?php
 include_once 'database.php';
 // insert a record in database
  if(isset($_POST['save']))
 {	 
-	 $name = $_POST['name'];
-	 $num_seed = $_POST['number of seed'];
-	 $sql = "INSERT INTO seec_record (name,number_of_seed)
+	 $name = $_POST['sname'];
+	 $num_seed = $_POST['qseeds'];
+	 $sql = "INSERT INTO seed_record (name,quantity)
 	 VALUES ('$name','$num_seed')";
 	 if (mysqli_query($conn, $sql)) {
 			echo "New record created successfully !";
